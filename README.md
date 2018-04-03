e7ff3ced8d6c1987f3744e761060fedd6a0666e1
ouija <mail@ouija.xyz>
2018-04-02 21:42:52 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# WC_Gateway_DOD
+WooCommerce Debit on Delivery
+
+Extends WooCommerce by adding a payment option of debit on delivery.
---
+++ wc-gateway-dod.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * Plugin Name: WooCommerce Debit on Delivery
+ * Plugin URI: https://github.com/ouija/WC_Gateway_DOD
+ * Description: Extends WooCommerce by adding a payment option of debit on delivery.
+ * Version: 1.0
+ * Requires at least: 4.0
+ * Tested up to: 4.9
+ * WC requires at least: 2.5
+ * WC tested up to: 3.3
+ * Author: ouija
+ * Author URI: http://ouija.xyz
+ * License: GPLv2 or later
+ * Text Domain: dod
+ */
+
+/* Initalization */
+add_action( 'plugins_loaded', 'dod_init', 0 );
+function dod_init() {
+
+	/* Checks if WooCommerce is installed and activated */
+	if ( ! class_exists( 'WC_Payment_Gateway' ) ) return;
+	
+	/* Inlcude payment gateway class */
+	include_once( 'class-wc-gateway-dod.php' );
+
+	/* Adds dod payment gateway */
+	add_filter( 'woocommerce_payment_gateways', 'dod_add_gateway' );
+	function dod_add_gateway( $methods ) {
+		$methods[] = 'WC_Gateway_DOD';
+		return $methods;
+	}
+}
+
+/* Plugin action links */
+add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'dod_action_links' );
+function dod_action_links( $links ) {
+	$plugin_links = array(
+		'<a href="' . admin_url( 'admin.php?page=wc-settings&tab=checkout&section=dod' ) . '">' . __( 'Settings', 'dod' ) . '</a>',
+	);
+	return array_merge( $plugin_links, $links );	
+}
